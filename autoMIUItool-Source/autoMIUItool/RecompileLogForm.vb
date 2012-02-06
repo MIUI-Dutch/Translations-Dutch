0aefa2592fa9a22a7ef1a071458ada0dbe1aec46
Kees-Jan Logemann <GitHub@KJLogic.net>
2012-02-06 20:42:22 +0100

    2.2.3 strings

--- autoMIUItool-Source/autoMIUItool/RecompileLogForm.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class RecompileLogForm
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        TextBox1.Text = autoMIUItool.RecompileLog
-    End Sub
-End Class
